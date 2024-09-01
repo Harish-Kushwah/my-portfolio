daf1ea822230065424fac1a60f86a3df0bd7e6e6
Harish-Kushwah <harishkushwah54321@gmail.com>
2024-09-01 20:46:23 +0530

    updated

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -13,7 +13,7 @@ import Error404 from "../pages/errors/error404/Error";
 export default class Main extends Component {
   render() {
     return (
-      <HashRouter basename="/my-portfolio">
+      <HashRouter basename="/">
         <Switch>
           <Route
             path="/"
